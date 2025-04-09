7dd6b5942295d7ddd27632cb6c036ead487a7105
Hristijan Gjorgjioski <hristijangjorgjioski@Hristijans-MacBook-Pro.local>
2025-04-09 19:55:14 +0200

    BinaryTreeInOrder

---
+++ algs/binary_tree_in_order.ts
@@ -0,0 +1,27 @@
+type BinaryNode<T> = {
+  value: T;
+  left: BinaryNode<T>;
+  right: BinaryNode<T>;
+};
+
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+  if (!curr) {
+    return path;
+  }
+
+  // recurse
+  walk(curr.left, path);
+
+  // pre
+  path.push(curr.value);
+
+  // recurse
+  walk(curr.right, path);
+
+  // post
+  return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+  return walk(head, []);
+}
